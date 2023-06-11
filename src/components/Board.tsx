30b7e2a3b9fda58ec5f15998f6b6100304d7b235
yaohong6782 <yaohong6782@gmail.com>
2023-06-11 17:25:44 +0800

    game structure development

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Card from "@/components/Card";
 
 import "@/scss/board.scss";
@@ -21,31 +21,79 @@ const Board = (props: BoardProps) => {
   const [shouldDisableAllCards, setShouldDisableAllCards] =
     useState<boolean>(false);
 
+  const timeout = useRef(setTimeout(() => {}));
+
+  const disable = () => {
+    setShouldDisableAllCards(true);
+  };
+
+  const enable = () => {
+    setShouldDisableAllCards(false);
+  };
+
+  const evalute = () => {
+    const [first, second] = openCards;
+    enable();
+
+    // check if both cards opened are equal
+    if ((first % 6) + 1 === (second % 6) + 1) {
+      console.log("matched");
+      setClearedCards((prev) => [...prev, first, second]);
+      setOpenCards([]);
+      return;
+    }
+
+    timeout.current = setTimeout(() => {
+      setOpenCards([]);
+    }, 500);
+  };
+
+  useEffect(() => {
+    let timeout = setTimeout(() => {});
+    if (openCards.length === 2) {
+        timeout = setTimeout(evalute, 300);
+    }
+    return () => {
+        clearTimeout(timeout);
+    }
+  },[openCards])
+
+  useEffect(() => {
+    if (clearedCards.length === props.cardIds.length){
+        console.log("game over!")
+    }
+  },[clearedCards])
   const handleCardClick = (id: number) => {
     if (openCards.length === 1) {
       setOpenCards((prev) => [...prev, id]);
+      disable();
     } else {
       setOpenCards([id]);
+      enable();
     }
   };
 
-  const checkIsFlipped = (id:number) => {
+  const checkIsFlipped = (id: number) => {
     return clearedCards.includes(id) || openCards.includes(id);
-  }
+  };
+
+  const checkIsInactive = (id: number) => {
+    return clearedCards.includes(id);
+  };
   return (
     <div className={"board"}>
       {props.cardIds.map((i, idx: number) => {
-        const imageName = imageNames[idx % imageNames.length];
+        const imageName = imageNames[i % imageNames.length];
         const imagePath = `src/assets/omok-imgs/${imageName}`;
         return (
           <Card
-            key={i}
+            key={idx}
             image={imagePath}
-            id={idx}
-            isDisabled={true}
-            isInactive={false}
-            isFlipped={true}
-            onClick={() => {}}
+            id={i}
+            isDisabled={shouldDisableAllCards}
+            isInactive={checkIsInactive(i)}
+            isFlipped={checkIsFlipped(i)}
+            onClick={handleCardClick}
           />
         );
       })}
